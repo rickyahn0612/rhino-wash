14f8c43152992481dc441287e5b02e2bd913b705
rickyahn0612 <rickyahn0612@yahoo.com>
2014-05-22 15:23:03 -0700

    Site updated to d22f405

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,10 @@
 (function() {
   $(function() {
+    $('html, body').fadeIn(1000);
+    $('.banner-contents-container').animate({
+      'right': 0,
+      'opacity': 1
+    }, 2000);
     $('.callboxes').on('mouseenter', function(e) {
       $(this).find('.services-name-box').animate({
         backgroundColor: "#D2583E"
@@ -14,10 +19,21 @@
       $(this).find('.services-name-box').css("color", "black");
       return $(this).find('.callboxes-overlay').fadeOut();
     });
-    return $(document).scroll(function() {
+    return $(window).scroll(function() {
       var winScroll;
-      winScroll = $(document).scrollTop();
-      return console.log(winScroll);
+      winScroll = $(window).scrollTop();
+      if (winScroll > 200) {
+        $('.callboxes-list').animate({
+          'top': 0,
+          'opacity': 1
+        }, 1000);
+      }
+      if (winScroll > 1200) {
+        return $('.about-list').animate({
+          'top': 50,
+          'opacity': 1
+        }, 2000);
+      }
     });
   });
 
