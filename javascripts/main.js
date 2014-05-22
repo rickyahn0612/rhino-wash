16e491a739a93de4031b7d9fa5e24f3b0986618a
rickyahn0612 <rickyahn0612@yahoo.com>
2014-05-22 15:58:48 -0700

    Site updated to 3587e6a

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,6 @@
 (function() {
   $(function() {
+    var state;
     $('html, body').fadeIn(1000);
     $('.banner-contents-container').animate({
       'right': 0,
@@ -18,6 +19,20 @@
       }, 400);
       $(this).find('.services-name-box').css("color", "black");
       return $(this).find('.callboxes-overlay').fadeOut();
+    });
+    state = false;
+    $('#tablet-nav').on('click', function(e) {
+      if (state === false) {
+        $('.drop-down').animate({
+          'height': 200
+        }, 0);
+        return state = true;
+      } else {
+        $('.drop-down').animate({
+          'height': 0
+        }, 0);
+        return state = false;
+      }
     });
     return $(window).scroll(function() {
       var winScroll;
